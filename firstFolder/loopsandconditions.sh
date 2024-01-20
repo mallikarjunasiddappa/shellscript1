0c0aa0c1c067b2105507e7e4f3816bc81b01888f
Mallikarjuna Siddappa <mallikarjuna.siddappa@systemverification.com>
2024-01-20 22:53:44 +0100

    adding loops and controls statement file

---
+++ firstFolder/loopsandconditions.sh
@@ -0,0 +1,15 @@
+a=4
+b=10
+
+if [ $a > $b ]
+then
+        echo " a is greaterthen b"
+else
+        echo "b is greaterthan a"
+
+fi
+
+#for loos
+
+for i in {1.100}; do echo $1; done
+
--- firstFolder/nodehealth.sh
+++ firstFolder/nodehealth.sh
@@ -6,8 +6,21 @@
 # This scripts outputs the node health
 ########################################################
 
+# Instead of writing echo on each line use debug mode
+
+set -x #Debug mode
+#set -e #Exit the script when there is an error in script
+#set -o pipefail # use set -o with -e because -e will not exit when comand with pipe
+# We can also write above set commands combined together like below
+#set -exo pipefail
+
+#echo "print the disk space"
 df -h
 
+#echo "print the memory"
 free -h
 
+#echo "print the cpu"
 nproc
+
+ps -ef | grep "usr"| awk -F" " '{print $2}'
