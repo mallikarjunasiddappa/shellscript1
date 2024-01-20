ecc5fb8f1065533174e26f096355492827a61b9d
Mallikarjuna Siddappa <mallikarjuna.siddappa@systemverification.com>
2024-01-20 19:56:11 +0100

    adding shellacript practice files

---
+++ firstFolder/nodehealth.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+########################################################
+# Author: MAllikarjuna
+# Date: 20/01/2024
+# Version: v0.2
+# This scripts outputs the node health
+########################################################
+
+df -h
+
+free -h
+
+nproc
---
+++ firstFolder/shellscript.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+##############################################
+# Author: Mallikarjuna
+# Date: 20/01/2024
+#
+# Sample script to learn shellscript
+# Version: v0.1
+##############################################
+# create a folder
+mkdir shellscriptfolder
+# create  files 
+cd shellscriptfolder
+touch firstfile secondfile
